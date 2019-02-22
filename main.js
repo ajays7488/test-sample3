09b9c144db9add4f8ce8402f31f4bbfbc2a49485
ajays7488 <42312922+ajays7488@users.noreply.github.com>
2019-02-22 12:40:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+<link href='https://fonts.googleapis.com/css?family=Dosis|Candal' rel='stylesheet' type='text/css'>
+<div class="toolbar">
+  <a href="#" data-command='undo'><i class='fa fa-undo'></i></a>
+  <a href="#" data-command='redo'><i class='fa fa-repeat'></i></a>
+  <div class="fore-wrapper"><i class='fa fa-font' style='color:#C96;'></i>
+    <div class="fore-palette">
+    </div>
+  </div>
+  <div class="back-wrapper"><i class='fa fa-font' style='background:#C96;'></i>
+    <div class="back-palette">
+    </div>
+  </div>  
+  <a href="#" data-command='createlink'><i class='fa fa-link'></i></a>
+  <a href="#" data-command='unlink'><i class='fa fa-unlink'></i></a>
+  <a href="#" data-command='insertimage'><i class='fa fa-image'></i></a>
+  <a href="#" data-command='subscript'><i class='fa fa-subscript'></i></a>
+  <a href="#" data-command='superscript'><i class='fa fa-superscript'></i></a>
+</div>
+<div id='editor' contenteditable>
+  <h1>A Heading.</h1>
+  <p>Some text that you can format. Try inserting an image or a link.</p>
+</div>
